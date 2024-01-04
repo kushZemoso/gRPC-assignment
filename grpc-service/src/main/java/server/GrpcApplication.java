f0d37b9fa7f75f5b574b06c834d7f9a4ed7f1d00
kushZemoso <kushagra.jain@zemosolabs.com>
2024-01-04 18:25:22 +0530

    feat:grps assignment.

---
+++ grpc-service/src/main/java/server/GrpcApplication.java
@@ -0,0 +1,11 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcApplication.class, args);
+    }
+}
